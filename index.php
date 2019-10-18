01a2b4663b9fd2ad0d526f5b1e043b77ab6dcff6
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2019-10-18 16:07:02 +0300

    array

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$maisto_produktai = [
+    [
+        'pavadinimas' => 'kiausai',
+        'dydis' => 'didelis',
+        'kiekis' => 2,
+    ],
+    [
+     'pavadinimas' => 'baklazanai',
+        'dydis' => 'mazas',
+        'kiekis' => 1,   
+    ],
+    [
+        'pavadinimas' => 'grietines_indelis',
+        'dydis' => 'didelis',
+        'kiekis' => 2,
+    ],
+ ];
+ var_dump($maisto_produktai);
+
+
+?>
+<html>
+    <head>
+        <title>Class Work</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
